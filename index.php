aba66197a855c469fda99ba1b3f72bbde2aba95f
norislay <norislay-14@hotmail.com>
2015-07-28 20:37:28 -0500

    correccion de la q

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "CONEXION A LA BASE DE DATOS";
 $URL ="sql5.freemysqlhosting.net";
 $database = "sql585297";
-$usuario = "ql585297";
+$usuario = "sql585297";
 $password = "wW7*jF7%";
 $port = 3306;
 // CREAR CONNECCION
